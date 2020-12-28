f67d237ab1fe987f1cfb13cf7997a21618efb3f7
Rok <hexgen.games@gmail.com>
2020-12-28 17:17:14 -0600

    Programming The Basic Hero Movement

---
+++ Assets/Scripts/Animator/AnimatorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public enum AnimationId { 
+    Idle=0,
+    Run=1,
+    Attack=2,
+    Hurt=3,
+    Jump=4
+}
+public class AnimatorController : MonoBehaviour
+{
+    private Animator animator;
+    public void Play(AnimationId animationId) {
+        if (animator == null) {
+            animator = GetComponent<Animator>();
+
+        }
+        animator.Play(animationId.ToString());
+    }
+}
---
+++ Assets/Scripts/Hero/HeroController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroController : MonoBehaviour
+{
+    [Header("Animation")]
+    [SerializeField] AnimatorController animatorController;
+
+    [SerializeField] float speed;
+
+
+    [Header("Hero States")]
+    public bool isFacingRight = false;
+
+    private Rigidbody2D _rigidbody2D;
+    private Vector2 movementDirection;
+    private Vector2 nonZeroMovementDirection;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        nonZeroMovementDirection = Vector2.right;
+        animatorController.Play(AnimationId.Idle);
+
+    }
+    void Update()
+    {
+        HandleControls();
+        HandleFacingDirection();
+        HandleHeroMovement();
+
+    }
+    void HandleFacingDirection() {
+        if (nonZeroMovementDirection.x > 0)
+        {
+            isFacingRight = true;
+            this.transform.rotation = Quaternion.identity;
+        }
+        else {
+            isFacingRight = false;
+            this.transform.rotation = Quaternion.Euler(0,180,0);
+
+        }
+    }
+    void HandleControls() {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+        if (Mathf.Abs(movementDirection.x) > 0)
+        {
+            animatorController.Play(AnimationId.Run);
+
+            nonZeroMovementDirection.x = movementDirection.x;
+        }
+        else {
+            animatorController.Play(AnimationId.Idle);
+
+        }
+        if (Mathf.Abs(movementDirection.y) > 0)
+        {
+            nonZeroMovementDirection.y = movementDirection.y;
+        }
+    }
+    void HandleHeroMovement() {
+        _rigidbody2D.velocity = new Vector2(movementDirection.x * speed, _rigidbody2D.velocity.y);
+
+
+
+    }
+
+
+}
