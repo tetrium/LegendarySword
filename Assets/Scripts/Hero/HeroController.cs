a8d91e39ea0dec28517a8fc42c232b8d2f69679b
Rok <hexgen.games@gmail.com>
2020-12-28 18:54:50 -0600

    Creating The Hero Animations

--- Assets/Scripts/Hero/HeroController.cs
+++ Assets/Scripts/Hero/HeroController.cs
@@ -8,12 +8,26 @@ public class HeroController : MonoBehaviour
     [SerializeField] AnimatorController animatorController;
 
     [SerializeField] float speed;
-
+    [SerializeField] float jumpForce;
 
     [Header("Hero States")]
     public bool isFacingRight = false;
+    public bool heroIsOnGround = false;
+    public bool heroIsJumping = false;
+    [Header("Interruption Variables")]
+
+
+
+    [Header("Layer Checkers")]
+    [SerializeField] LayerChecker footA;
+    [SerializeField] LayerChecker footB;
+
 
     private Rigidbody2D _rigidbody2D;
+
+
+    //Control Variables
+    private bool jumpButtonPressed = false;
     private Vector2 movementDirection;
     private Vector2 nonZeroMovementDirection;
 
@@ -27,10 +41,34 @@ public class HeroController : MonoBehaviour
     void Update()
     {
         HandleControls();
+        CheckGround();
         HandleFacingDirection();
         HandleHeroMovement();
+        HandleJump();
+    }
+    void HandleJump() {
+        if (jumpButtonPressed && heroIsOnGround&&!heroIsJumping) {
+            _rigidbody2D.velocity = Vector2.zero;
+            _rigidbody2D.AddForce(Vector2.up*jumpForce,ForceMode2D.Impulse);
+            StartCoroutine(HandleJumpAnimation());
+        }
+        if (heroIsJumping&&heroIsOnGround) {
+            heroIsJumping = false;
+        }
+       
+    }
+
+    IEnumerator HandleJumpAnimation() {
+        yield return new WaitForSeconds(0.2f);
+        heroIsJumping = true;
+        animatorController.Play(AnimationId.Jump);
 
     }
+
+    void CheckGround() {
+        heroIsOnGround = footA.isTouching || footB.isTouching;
+    }
+   
     void HandleFacingDirection() {
         if (nonZeroMovementDirection.x > 0)
         {
@@ -45,16 +83,13 @@ public class HeroController : MonoBehaviour
     }
     void HandleControls() {
         movementDirection = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
-        if (Mathf.Abs(movementDirection.x) > 0)
-        {
-            animatorController.Play(AnimationId.Run);
+        jumpButtonPressed = Input.GetButtonDown("Jump");
 
-            nonZeroMovementDirection.x = movementDirection.x;
-        }
-        else {
-            animatorController.Play(AnimationId.Idle);
+     
 
-        }
+
+
+
         if (Mathf.Abs(movementDirection.y) > 0)
         {
             nonZeroMovementDirection.y = movementDirection.y;
@@ -63,8 +98,20 @@ public class HeroController : MonoBehaviour
     void HandleHeroMovement() {
         _rigidbody2D.velocity = new Vector2(movementDirection.x * speed, _rigidbody2D.velocity.y);
 
+        if (!heroIsJumping && heroIsOnGround)
+        {
+            if (Mathf.Abs(movementDirection.x) > 0)
+            {
+                animatorController.Play(AnimationId.Run);
 
+                nonZeroMovementDirection.x = movementDirection.x;
+            }
+            else
+            {
+                animatorController.Play(AnimationId.Idle);
 
+            }
+        }
     }
 
 
---
+++ Assets/Scripts/Physics/LayerChecker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerChecker : MonoBehaviour
+{
+    [SerializeField] LayerMask targetMask;
+    [SerializeField] Vector2 direction;
+    [SerializeField] float distance = 2;
+
+    private bool _isTouching;
+    public bool isTouching
+    {
+        get {
+            _isTouching = Physics2D.Raycast(this.transform.position, direction, distance, targetMask);
+            return _isTouching;
+        }
+
+       
+    
+    }
+
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        if (isTouching)
+        {
+            Gizmos.color = Color.green;
+        }
+        else {
+            Gizmos.color = Color.red;
+        }
+
+        Gizmos.DrawRay(this.transform.position, direction * distance);
+    }
+
+#endif
+
+}
